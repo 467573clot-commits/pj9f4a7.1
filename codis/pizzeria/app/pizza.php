0076be1125415d06e292f0f23fd2b9ea44765669
467573clot-commits <467573.clot@fje.edu>
2025-11-08 18:05:01 +0100

    Repositorio limpio sin archivos grandes

---
+++ codis/pizzeria/app/pizza.php
@@ -0,0 +1,21 @@
+<?php
+	$pvp = 10;
+	$IVA = 21;
+
+	$pvp_public = $pvp * ((100 + $IVA) / 100);
+	echo "El preu de la pizza bàsica és $pvp_public € (IVA inclòs)<br>";
+
+	if (empty($_GET['ingr'])) {
+		echo "Has demanat només la pizza bàsica.<br>";
+	}
+	else {
+		$pvp_public = ((100 + $IVA) / 100) * ($pvp + count($_GET['ingr']));
+		echo "Has demanat la pizza bàsica amb:<br>";
+		foreach ($_GET['ingr'] as $ingredient) {
+			if ($ingredient) {
+				echo "* $ingredient<br>";
+			}
+		}
+		echo "El preu és $pvp_public € (IVA inclòs)<br>";
+	}
+?>
